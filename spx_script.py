993899a11995ee432614adf5b0db571fcb13f4b1
tolgagok-tech <tolgagokcenn@gmail.com>
2026-02-14 17:08:15 +0300

    Update spx_script.py

--- spx_script.py
+++ spx_script.py
@@ -3,26 +3,23 @@ import pandas as pd
 import numpy as np
 import time
 
-# --- 1. SEMBOL LİSTESİ ---
-# .IS ekleri kaldırılmış temiz liste
+# --- DOSYANIZDAKİ HİSSE LİSTESİ ---
 semboller = [
     "A", "AA", "AAL", "AAON", "AAPL", "ABBV", "ABNB", "ABT", "ACGL", "ACHC", "ACI", "ACM", "ACN", "ADBE", "ADC", "ADI", "ADM", "ADP", "ADSK", "ADT", "AEE", "AEP", "AES", "AFG", "AFL", "AFRM", "AGCO", "AGNC", "AGO", "AIG", "AIT", "AIZ", "AJG", "AKAM", "AL", "ALAB", "ALB", "ALGM", "ALGN", "ALK", "ALL", "ALLE", "ALLY", "ALNY", "ALSN", "AM", "AMAT", "AMCR", "AMD", "AME", "AMG", "AMGN", "AMH", "AMKR", "AMP", "AMT", "AMTM", "AMZN", "AN", "ANET", "AON", "AOS", "APA", "APD", "APG", "APH", "APLS", "APO", "APP", "APPF", "APTV", "AR", "ARE", "ARES", "ARMK", "ARW", "AS", "ASH", "ASTS", "ATI", "ATO", "ATR", "AU", "AUR", "AVB", "AVGO", "AVT", "AVTR", "AVY", "AWI", "AWK", "AXON", "AXP", "AXS", "AXTA", "AYI", "AZO", "BA", "BAC", "BAH", "BALL", "BAM", "BAX", "BBWI", "BBY", "BC", "BDX", "BEN", "BEPC", "BFA", "BFAM", "BFB", "BG", "BHF", "BIIB", "BILL", "BIO", "BIRK", "BJ", "BK", "BKNG", "BKR", "BLD", "BLDR", "BLK", "BLSH", "BMRN", "BMY", "BOKF", "BPOP", "BR", "BRBR", "BRKB", "BRKR", "BRO", "BROS", "BRX", "BSX", "BSY", "BURL", "BWA", "BWXT", "BX", "BXP", "BYD", "C", "CACC", "CACI", "CAG", "CAH", "CAI", "CAR", "CARR", "CART", "CASY", "CAT", "CAVA", "CB", "CBOE", "CBRE", "CBSH", "CCC", "CCI", "CCK", "CCL", "CDNS", "CDW", "CE", "CEG", "CELH", "CERT", "CF", "CFG", "CFLT", "CFR", "CG", "CGNX", "CHD", "CHDN", "CHE", "CHH", "CHRD", "CHRW", "CHTR", "CHWY", "CI", "CIEN", "CINF", "CL", "CLF", "CLH", "CLVT", "CLX", "CMCSA", "CME", "CMG", "CMI", "CMS", "CNA", "CNC", "CNH", "CNM", "CNP", "CNXC", "COF", "COHR", "COIN", "COKE", "COLB", "COLD", "COLM", "COO", "COP", "COR", "CORT", "COST", "COTY", "CPAY", "CPB", "CPNG", "CPRT", "CPT", "CR", "CRCL", "CRH", "CRL", "CRM", "CROX", "CRS", "CRUS", "CRWD", "CSCO", "CSGP", "CSL", "CSX", "CTAS", "CTRA", "CTSH", "CTVA", "CUBE", "CUZ", "CVNA", "CVS", "CVX", "CW", "CWEN", "CWENA", "CXT", "CZR", "D", "DAL", "DAR", "DASH", "DBX", "DCI", "DD", "DDOG", "DDS", "DE", "DECK", "DELL", "DG", "DGX", "DHI", "DHR", "DINO", "DIS", "DJT", "DKNG", "DKS", "DLB", "DLR", "DLTR", "DOC", "DOCS", "DOCU", "DOV", "DOW", "DOX", "DPZ", "DRI", "DRS", "DT", "DTE", "DTM", "DUK", "DUOL", "DV", "DVA", "DVN", "DXC", "DXCM", "EA", "EBAY", "ECG", "ECL", "ED", "EEFT", "EFX", "EG", "EGP", "EHC", "EIX", "EL", "ELAN", "ELF", "ELS", "ELV", "EME", "EMN", "EMR", "ENPH", "ENTG", "EOG", "EPAM", "EPR", "EQH", "EQIX", "EQR", "EQT", "ES", "ESAB", "ESI", "ESS", "ESTC", "ETN", "ETR", "ETSY", "EVR", "EVRG", "EW", "EWBC", "EXAS", "EXC", "EXE", "EXEL", "EXLS", "EXP", "EXPD", "EXPE", "EXR", "F", "FAF", "FANG", "FAST", "FBIN", "FCN", "FCNCA", "FCX", "FDS", "FDX", "FE", "FERG", "FFIV", "FHB", "FHN", "FICO", "FIGR", "FIS", "FISV", "FITB", "FIVE", "FIX", "FLEX", "FLO", "FLS", "FLUT", "FMC", "FNB", "FND", "FNF", "FOUR", "FOX", "FOXA", "FR", "FRHC", "FRMI", "FRPT", "FRT", "FSLR", "FTAI", "FTI", "FTNT", "FTV", "FWONA", "FWONK", "G", "GAP", "GD", "GDDY", "GE", "GEHC", "GEN", "GEV", "GFS", "GGG", "GILD", "GIS", "GL", "GLIBA", "GLIBK", "GLOB", "GLPI", "GLW", "GM", "GME", "GMED", "GNRC", "GNTX", "GOOG", "GOOGL", "GPC", "GPK", "GPN", "GRMN", "GS", "GTES", "GTLB", "GTM", "GWRE", "GWW", "GXO", "H", "HAL", "HALO", "HAS", "HAYW", "HBAN", "HCA", "HD", "HEI", "HEIA", "HHH", "HIG", "HII", "HIW", "HLI", "HLNE", "HLT", "HOG", "HOLX", "HON", "HOOD", "HPE", "HPQ", "HR", "HRB", "HRL", "HSIC", "HST", "HSY", "HUBB", "HUBS", "HUM", "HUN", "HWM", "HXL", "IAC", "IBKR", "IBM", "ICE", "IDA", "IDXX", "IEX", "IFF", "ILMN", "INCY", "INGM", "INGR", "INSM", "INSP", "INTC", "INTU", "INVH", "IONS", "IOT", "IP", "IPGP", "IQV", "IR", "IRDM", "IRM", "ISRG", "IT", "ITT", "ITW", "IVZ", "J", "JAZZ", "JBHT", "JBL", "JCI", "JEF", "JHG", "JHX", "JKHY", "JLL", "JNJ", "JPM", "KBR", "KD", "KDP", "KEX", "KEY", "KEYS", "KHC", "KIM", "KKR", "KLAC", "KMB", "KMI", "KMPR", "KMX", "KNSL", "KNX", "KO", "KR", "KRC", "KRMN", "KVUE", "L", "LAD", "LAMR", "LAZ", "LBRDA", "LBRDK", "LBTYA", "LBTYK", "LCID", "LDOS", "LEA", "LECO", "LEN", "LENB", "LFUS", "LH", "LHX", "LII", "LIN", "LINE", "LITE", "LKQ", "LLY", "LLYVA", "LLYVK", "LMT", "LNC", "LNG", "LNT", "LOAR", "LOPE", "LOW", "LPLA", "LPX", "LRCX", "LSCC", "LSTR", "LULU", "LUV", "LVS", "LW", "LYB", "LYFT", "LYV", "M", "MA", "MAA", "MAN", "MANH", "MAR", "MAS", "MASI", "MAT", "MCD", "MCHP", "MCK", "MCO", "MDB", "MDLZ", "MDT", "MDU", "MEDP", "MET", "META", "MGM", "MHK", "MIDD", "MKC", "MKL", "MKSI", "MKTX", "MLI", "MLM", "MMM", "MNST", "MO", "MOH", "MORN", "MOS", "MP", "MPC", "MPT", "MPWR", "MRK", "MRNA", "MRP", "MRSH", "MRVL", "MS", "MSA", "MSCI", "MSFT", "MSGS", "MSI", "MSM", "MSTR", "MTB", "MTCH", "MTD", "MTDR", "MTG", "MTN", "MTSI", "MTZ", "MU", "MUSA", "NBIX", "NCLH", "NCNO", "NDAQ", "NDSN", "NEE", "NEM", "NET", "NEU", "NFG", "NFLX", "NI", "NIQ", "NKE", "NLY", "NNN", "NOC", "NOV", "NOW", "NRG", "NSA", "NSC", "NTAP", "NTNX", "NTRA", "NTRS", "NU", "NUE", "NVDA", "NVR", "NVST", "NVT", "NWL", "NWS", "NWSA", "NXST", "NYT", "O", "OC", "ODFL", "OGE", "OGN", "OHI", "OKE", "OKTA", "OLED", "OLLI", "OLN", "OMC", "OMF", "ON", "ONON", "ONTO", "ORCL", "ORI", "ORLY", "OSK", "OTIS", "OVV", "OWL", "OXY", "OZK", "PAG", "PANW", "PATH", "PAYC", "PAYX", "PB", "PCAR", "PCG", "PCOR", "PCTY", "PEG", "PEGA", "PEN", "PENN", "PEP", "PFE", "PFG", "PFGC", "PG", "PGR", "PH", "PHM", "PINS", "PK", "PKG", "PLD", "PLNT", "PLTR", "PM", "PNC", "PNFP", "PNR", "PNW", "PODD", "POOL", "POST", "PPC", "PPG", "PPL", "PR", "PRGO", "PRI", "PRMB", "PRU", "PSA", "PSN", "PSTG", "PSX", "PTC", "PVH", "PWR", "PYPL", "Q", "QCOM", "QGEN", "QRVO", "QS", "QSR", "QXO", "R", "RAL", "RARE", "RBA", "RBC", "RBLX", "RBRK", "RCL", "RDDT", "REG", "REGN", "REXR", "REYN", "RF", "RGA", "RGEN", "RGLD", "RH", "RHI", "RITM", "RIVN", "RJF", "RKLB", "RKT", "RL", "RLI", "RMD", "RNG", "RNR", "ROIV", "ROK", "ROKU", "ROL", "ROP", "ROST", "RPM", "RPRX", "RRC", "RRX", "RS", "RSG", "RTX", "RVMD", "RVTY", "RYAN", "RYN", "S", "SAIA", "SAIC", "SAIL", "SAM", "SARO", "SBAC", "SBUX", "SCCO", "SCHW", "SCI", "SEB", "SEE", "SEIC", "SF", "SFD", "SFM", "SGI", "SHC", "SHW", "SIRI", "SITE", "SJM", "SLB", "SLGN", "SLM", "SMCI", "SMG", "SMMT", "SN", "SNA", "SNDK", "SNDR", "SNOW", "SNPS", "SNX", "SO", "SOFI", "SOLS", "SOLV", "SON", "SPG", "SPGI", "SPOT", "SRE", "SRPT", "SSB", "SSD", "SSNC", "ST", "STAG", "STE", "STLD", "STT", "STWD", "STZ", "SUI", "SW", "SWK", "SWKS", "SYF", "SYK", "SYY", "TAP", "TDC", "TDG", "TDY", "TEAM", "TECH", "TEM", "TER", "TFC", "TFSL", "TFX", "TGT", "THC", "THG", "THO", "TIGO", "TJX", "TKO", "TKR", "TLN", "TMO", "TMUS", "TNL", "TOL", "TOST", "TPG", "TPL", "TPR", "TREX", "TRGP", "TRMB", "TROW", "TRU", "TRV", "TSCO", "TSLA", "TSN", "TT", "TTC", "TTD", "TTEK", "TTWO", "TW", "TWLO", "TXN", "TXRH", "TXT", "TYL", "U", "UA", "UAA", "UAL", "UBER", "UDR", "UGI", "UHAL", "UHALB", "UHS", "UI", "ULTA", "UNH", "UNM", "UNP", "UPS", "URI", "USB", "USFD", "UTHR", "UWMC", "V", "VEEV", "VFC", "VICI", "VIK", "VIRT", "VKTX", "VLO", "VLTO", "VMC", "VMI", "VNO", "VNOM", "VNT", "VOYA", "VRSK", "VRSN", "VRT", "VRTX", "VSNT", "VST", "VTR", "VTRS", "VVV", "VZ", "WAB", "WAL", "WAT", "WBD", "WBS", "WCC", "WDAY", "WDC", "WEC", "WELL", "WEN", "WEX", "WFC", "WFRD", "WH", "WHR", "WING", "WLK", "WM", "WMB", "WMS", "WMT", "WPC", "WRB", "WSC", "WSM", "WSO", "WST", "WTFC", "WTM", "WTRG", "WTW", "WU", "WWD", "WY", "WYNN", "XEL", "XOM", "XP", "XPO", "XRAY", "XYL", "XYZ", "YETI", "YUM", "Z", "ZBH", "ZBRA", "ZG", "ZION", "ZM", "ZS", "ZTS"
 ]
 
-# --- 2. FONKSİYONLAR ---
 def get_price(ticker):
     try:
-        # download yerine daha stabil olan Ticker.history kullanıyoruz
-        data = yf.Ticker(ticker).history(period="2y", interval="1d", auto_adjust=True)
+        data = yf.download(ticker, period="2y", interval="1d", progress=False, auto_adjust=True)
         if data.empty: return None
-        return data['Close'].dropna()
+        close = data['Close'] if 'Close' in data.columns else data.iloc[:, 0]
+        if isinstance(close, pd.DataFrame): close = close.iloc[:, 0]
+        return close.dropna()
     except: return None
 
 def rs_hesapla(fiyatlar, end_perf_skor):
-    # En az 1 yıllık (252 işlem günü) veri şartı
     if fiyatlar is None or len(fiyatlar) < 252: return None
     try:
-        # Ağırlıklı RS Skoru: Son 3 ay (%40), 6, 9 ve 12 ay (%20şer) etkili
         skor = (0.4 * (fiyatlar.iloc[-1] / fiyatlar.iloc[-min(63, len(fiyatlar))])) + \
                (0.2 * (fiyatlar.iloc[-1] / fiyatlar.iloc[-min(126, len(fiyatlar))])) + \
                (0.2 * (fiyatlar.iloc[-1] / fiyatlar.iloc[-min(189, len(fiyatlar))])) + \
@@ -30,8 +27,7 @@ def rs_hesapla(fiyatlar, end_perf_skor):
         return (float(skor) / end_perf_skor) * 100
     except: return None
 
-# --- 3. ENDEKS (BENCHMARK) ANALİZİ ---
-print("Kıyaslama Endeksi (^GSPC - S&P 500) verisi alınıyor...")
+# Endeks Hazırlığı (^GSPC = S&P 500)
 spx_fiyat = get_price("^GSPC")
 if spx_fiyat is not None and len(spx_fiyat) >= 252:
     end_perf = (0.4 * (spx_fiyat.iloc[-1] / spx_fiyat.iloc[-min(63, len(spx_fiyat))])) + \
@@ -40,44 +36,29 @@ if spx_fiyat is not None and len(spx_fiyat) >= 252:
                (0.2 * (spx_fiyat.iloc[-1] / spx_fiyat.iloc[-min(252, len(spx_fiyat))]))
     end_perf = float(end_perf)
 else:
-    print("Endeks verisi alınamadı, standart çarpan 1.0 kullanılacak.")
     end_perf = 1.0
 
-# --- 4. ANA DÖNGÜ ---
 sonuclar = []
-toplam = len(semboller)
-print(f"Analiz Başladı. Toplam {toplam} hisse taranıyor...")
+print(f"S&P 500 Analizi Başlıyor. Toplam {len(semboller)} hisse taranacak...")
 
-for i, s in enumerate(semboller):
+for s in semboller:
     fiyat_serisi = get_price(s)
     if fiyat_serisi is not None:
         rs_val = rs_hesapla(fiyat_serisi, end_perf)
         if rs_val is not None:
             sonuclar.append({'Hisse': s, 'RS_Skoru': round(rs_val, 4)})
-    
-    # Her 50 hissede bir durum raporu
-    if (i + 1) % 50 == 0:
-        yuzde = round(((i + 1) / toplam) * 100, 1)
-        print(f"İlerleme: %{yuzde} ({i + 1}/{toplam})")
-    
-    # Yahoo Finance rate limitine takılmamak için kısa bekleme
     time.sleep(0.05)
 
-# --- 5. SONUÇLARI KAYDETME VE SIRALAMA ---
 if sonuclar:
     df = pd.DataFrame(sonuclar)
-    # RS Rating: En iyi %1'lik dilim 99 puan, en kötü %1'lik dilim 1 puan alır.
     df['RS_Rating'] = (df['RS_Skoru'].rank(pct=True) * 99).round(1)
     df = df.sort_values(by='RS_Rating', ascending=False)
     
-    # CSV'ye kaydet
-    dosya_adi = 'abd_hisseleri_rs_analizi.csv'
-    df.to_csv(dosya_adi, index=False, sep=';')
-    
-    print(f"\n--- ANALİZ TAMAMLANDI ---")
-    print(f"Başarıyla işlenen: {len(df)} hisse.")
-    print(f"Dosya kaydedildi: {dosya_adi}")
-    print("\nEn Güçlü 5 Hisse:")
-    print(df.head(5))
-else:
-    print("Hata: Hiçbir veri işlenemedi.")
+    print("\n--- TRADINGVIEW PARAMETRELERİ ---")
+    quantiles = [0.99, 0.90, 0.70, 0.50, 0.30, 0.10, 0.01]
+    for q in quantiles:
+        val = df['RS_Skoru'].quantile(q)
+        print(f"Quantile {int(q*100)}: {float(val):.4f}")
+
+    df.to_csv('spx_rs_siralamasi.csv', index=False, sep=';')
+    print(f"\nAnaliz tamamlandı. {len(df)} hisse işlendi.")
